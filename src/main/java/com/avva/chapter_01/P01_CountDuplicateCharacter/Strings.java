8f8041777389005f17a40c2a03df0d23acd5f21f
AVVA <abba501@mail.ru>
2025-12-07 11:06:54 +0300

    07.12.2025

--- src/main/java/com/avva/chapter_01/P01_CountDuplicateCharacter/Strings.java
+++ src/main/java/com/avva/chapter_01/P01_CountDuplicateCharacter/Strings.java
@@ -1,4 +1,41 @@
 package com.avva.chapter_01.P01_CountDuplicateCharacter;
 
-public class Strings {
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public final class Strings {
+
+    private Strings() {
+        throw new AssertionError("Cannot be instantiated");
+    }
+
+    public static Map<Character, Integer> countDuplicateCharactersV1(String str) {
+        if (str == null || str.isBlank()) {
+            // or throw IllegalArgumentException
+            return Collections.emptyMap();
+        }
+
+        Map<Character, Integer> result = new HashMap<>();
+
+        // or use for(char ch: str.toCharArray()) { ... }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
+        return result;
+    }
+
+    public static Map<Character, Long> countDuplicateCharactersV2(String str) {
+        if (str == null || str.isBlank()) {
+            // or throw IllegalArgumentException
+            return Collections.emptyMap();
+        }
+
+        Map<Character, Long> result = str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        return result;
+    }
 }
