9aab416cae91fe8d5f439e9ad2fd2cfdfc368d66
AVVA <abba501@mail.ru>
2025-12-18 11:20:39 +0300

    18.12.2025

--- src/main/java/com/avva/chapter_01/P01_CountDuplicateCharacter/Main.java
+++ src/main/java/com/avva/chapter_01/P01_CountDuplicateCharacter/Main.java
@@ -10,13 +10,19 @@ public class Main {
             + "And believe that anything is possible when you have the right "
             + "people there to support you. ";
 
+    // Ӝ -> Unicode: \u04DC, Code Point: 1244
+    // 💕 -> Unicode: \uD83D\uDC95, Code Point: 128149
+    // 🎼 -> \uD83C\uDFBC, Code Point: 127932
+    // 😍 ->\uD83D\uDE0D, Code Point: 128525
+    private static final String TEXT_CP = TEXT + "😍 I love 💕 you Ӝ so much 💕 😍 🎼🎼🎼!";
+
     public static void main(String[] args) {
 
         System.out.println("Input text: \n" + TEXT + "\n");
 
-        System.out.println("\n\nASCII or 16 bits Unicode characters (less than 65,535 (0xFFFF)) examples:\n");
+        System.out.println("ASCII or 16 bits Unicode characters (less than 65,535 (0xFFFF)) examples:\n");
 
-        System.out.println("HashMap based solution:");
+        System.out.println("-----------( HashMap based solution )-----------------");
         long startTimeV1 = System.nanoTime();
 
         Map<Character, Integer> duplicatesV1 = Strings.countDuplicateCharactersV1(TEXT);
@@ -36,7 +42,29 @@ public class Main {
         // or:
         duplicatesV2.forEach((k, v) -> System.out.print(k + "=" + v + ", "));
 
+        System.out.println("\n-----------------------------------------------------------");
 
+        System.out.println("Input text: \n" + TEXT_CP + "\n");
+        System.out.println("Including Unicode surrogate pairs examples:");
+        System.out.println("HashMap based solution:");
+        long startTimeV3 = System.nanoTime();
+
+        Map<String, Integer> duplicatesV3 = Strings.countDuplicateCharactersVCP1(TEXT_CP);
+
+        displayExecutionTime(System.nanoTime()-startTimeV3);
+        System.out.println(Arrays.toString(duplicatesV3.entrySet().toArray()));
+        // or: duplicatesV3.forEach( (k, v) -> System.out.print(k + "="+ v + ", "));
+
+        System.out.println("\n-----------------------------------------------------------");
+
+        System.out.println("Java 8, functional-style solution:");
+        long startTimeV4 = System.nanoTime();
+
+        Map<String, Long> duplicatesV4 = Strings.countDuplicateCharactersVCP2(TEXT_CP);
+
+        displayExecutionTime(System.nanoTime()-startTimeV4);
+        System.out.println(Arrays.toString(duplicatesV4.entrySet().toArray()));
+        // or: duplicatesV4.forEach( (k, v) -> System.out.print(k + "="+ v + ", "));
     }
 
     private static void displayExecutionTime(long time) {
